255379b12ae27fde4a6577003ee66c011ac11b40
Mitch Dzugan <mitch@Mitchs-MacBook-Pro.local>
2015-05-16 14:59:55 -0700

    Node server project setup

---
+++ server/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var users = [];
+
+app.use(express.static('../client/stream-of-redditness-app/www/'));
+
+
+app.get('/', function (req, res){
+  res.sendFile('../client/stream-of-redditness-app/www/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.on('login', function (name) {
+  });
+
+  socket.on('sendMessage', function (name, message) {
+  });
+
+  socket.on('disconnect', function () {
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
