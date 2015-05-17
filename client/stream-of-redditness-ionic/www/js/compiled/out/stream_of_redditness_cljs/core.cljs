24b2935a3b9c2152046061863903d5387153b77a
Mitch Dzugan <mitch@Mitchs-MacBook-Pro.local>
2015-05-17 05:56:12 -0700

    OAuth basics

--- client/stream-of-redditness-ionic/www/js/compiled/out/stream_of_redditness_cljs/core.cljs
+++ client/stream-of-redditness-ionic/www/js/compiled/out/stream_of_redditness_cljs/core.cljs
@@ -8,13 +8,43 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom { :text "Hello world!"
+                           :awaitingAuthVal false
+                           :authVal "FUCK YOU"
+                           :authIntervalId 0
+                           :authWorkCleared true}))
+
+(def socket (js/io "http://localhost:3000"))
+(.on socket "authVal" 
+     (fn [msg] (if (:awaitingAuthVal @app-state) 
+                 (swap! app-state (fn [state] (assoc state :authVal msg :awaitingAuthVal false))))))
+
+(defn onAuthNewClick [event]
+  (if (:authWorkCleared @app-state) 
+    (do
+      (swap! app-state (fn [state] (assoc state :awaitingAuthVal true :authWorkCleared false)))
+      (.emit socket "requestAuth")
+      (swap! app-state 
+             (fn [state] 
+               (assoc state :authIntervalId 
+                 (.setInterval 
+                   js/window 
+                   (fn [] 
+                     (if-not (:awaitingAuthVal @app-state) 
+                       (do
+                         (.clearInterval js/window (:authIntervalId @app-state))
+                         (.open js/window (:authVal @app-state) "_system")
+                         (swap! app-state (fn [state] (assoc state :authWorkCleared true))))))
+                   1000)))))))
 
 (om/root
   (fn [data owner]
     (reify om/IRender
       (render [_]
-        (dom/h1 nil (:text data)))))
+        (dom/div nil
+          (dom/button nil "Ayy lmao") (dom/br nil)
+          (dom/button #js {:id "AuthButton" :onClick onAuthNewClick} "Authorize New Account")
+          (dom/h2 nil (:text data))))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http');
+var request = require('request');
+var server = http.Server(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
 
-var users = [];
+auth = "Basic " + new Buffer(fs.readFileSync("auth")).toString("base64");
+
+var authorizing = {};
 
 app.use(express.static('../client/stream-of-redditness-ionic/www/'));
 
@@ -12,17 +17,59 @@ app.get('/', function (req, res){
   res.sendFile('../client/stream-of-redditness-ionic/www/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.on('login', function (name) {
-  });
+app.get("/auth", function (req, res) {
+  if (Object.keys(authorizing).indexOf(req.query.state) != -1) {
+  	request.post({
+		url: 'https://www.reddit.com/api/v1/access_token',
+		headers: {
+			"Authorization" : auth
+		},
+		form: {
+			grant_type: 'authorization_code', 
+			code: req.query.code,
+			redirect_uri: 'http://localhost:3000/auth'
+		}
+	}, function(err, httpResponse, body) {
+			console.log(httpResponse);
+			console.log(body);
+		}
+	);
+  }
+  res.send("Hello");
+});
 
-  socket.on('sendMessage', function (name, message) {
+io.on('connection', function (socket) {
+  console.log("A user Connected");
+
+  socket.on('requestAuth', function (name) {
+  	var authIdGen = function() {
+  		var randomTextGen = function() {
+			var text = "";
+			var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+			for( var i=0; i < 5; i++ )
+			    text += possible.charAt(Math.floor(Math.random() * possible.length));
+			return text;
+		};
+		var authId = randomTextGen();
+		while(Object.keys(authorizing).indexOf(authId) != -1) {
+			authId = randomTextGen();
+		}
+		authorizing[authId] = {socket: socket, time: Date.now()};
+		return authId;
+	};
+	var authUrl = "https://www.reddit.com/api/v1/authorize?"       + 
+	              "client_id=-rQFIlIkDCNYRA&response_type=code&"   +
+                  "state=" + authIdGen() + "&redirect_uri="        +
+                  "http://localhost:3000/auth&duration=permanent&" +
+                  "scope=identity,edit,save,submit,vote"
+  	socket.emit("authVal", authUrl);
   });
 
   socket.on('disconnect', function () {
+  console.log("A user Disconnected");
   });
 });
 
-http.listen(3000, function(){
+server.listen(3000, function(){
   console.log('listening on *:3000');
 });
