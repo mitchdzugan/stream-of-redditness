314cd15e6fa88af7d59e8c8852a3c3ba94171ae0
Mitchell Dzugan <mitchdzugan@gmail.com>
2015-05-22 13:01:38 -0700

    user Heroku port

--- server.js
+++ server.js
@@ -101,6 +101,6 @@ io.on('connection', function (socket) {
   });
 });
 
-server.listen(3000, function(){
+server.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
